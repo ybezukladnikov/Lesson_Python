1e8e19796069489b335443b224c619affdb82bf3
Yaroslav <yaros6666@yandex.ru>
2022-09-07 10:04:33 +0300

    Correct

--- Lesson_01/HomeWork/Task_02.py
+++ Lesson_01/HomeWork/Task_02.py
@@ -1,17 +1,15 @@
 # Задача №2. Напишите программу для. проверки истинности
 # утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-flag = True
+# print(f'X\t\tY\t\tZ\t\tResult')
+# print('*'*30)
+# for x in [True, False]:
+#     for y in [True, False]:
+#         for z in [True, False]:
+#             print(f'{x}\t{y}\t{z}\t{(not(x or y or z)) == (not x and not y and not z )} ')
+from random import randint
 
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            if x+y+z >=1: temp=1
-            else:temp=0
-            if 1-temp !=(1-x)*(1-y)*(1-z):flag = False
-
-if flag: print('Statement is true')
-else: print('Statement is false')
-
+array = [list(map(int,input("input: ").split())) for i in range(2)]
+print(array)
 
 
