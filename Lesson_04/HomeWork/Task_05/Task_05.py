941dfe57235347dce56e1566691cdd0a86cefb2d
Yaroslav_Bezukladnikov <ybezukladnikov@gmail.com>
2022-09-14 19:51:48 +0300

    Done 5 task

--- Lesson_04/HomeWork/Task_05/Task_05.py
+++ Lesson_04/HomeWork/Task_05/Task_05.py
@@ -6,37 +6,60 @@
 # файл второй:
 # сжатый текст.
 
-#
-text = "AAAAAAAAAAAA"
-new_t = ""
-count = 1
-index = 0
-flag = True
-while index<len(text)-1:
-    while text[index]==text[index+1]:
-        index+=1
-        count+=1
-        if index==len(text)-1:
-            break
 
-    new_t = new_t + str(count) + text[index]
-    count = 1
-    index += 1
-    if index == len(text) - 1:
-        new_t = new_t + str(count) + text[index]
+def compression():
+    '''
+    Функция считает повторяющиеся элементы.
+    И записывает сжатую строку в файл.
+    :return:
+    '''
+    with open('origin_text.txt', 'r') as file_obj:
+        text = file_obj.read()
+    compression_text = ""
+    count_letter = 1
+    position_sym = 0
 
-print(new_t)
-############
+    while position_sym<len(text)-1:
+        while text[position_sym]==text[position_sym+1]:
+            position_sym+=1
+            count_letter+=1
+            if position_sym==len(text)-1:
+                break
 
-text_1 = new_t
-index = 0
-res = ''
-while index < len(text_1)-1:
-    count = int(text_1[index])
-    index+=1
-    while count>0:
-        res+=text_1[index]
-        count-=1
-    index+=1
+        compression_text = compression_text + str(count_letter) + text[position_sym]
+        count_letter = 1
+        position_sym += 1
+        if position_sym == len(text) - 1:
+            compression_text = compression_text + str(count_letter) + text[position_sym]
 
-print(res)
+    with open('compression_text.txt', 'w') as file_obj:
+        file_obj.write(compression_text)
+
+compression()
+
+def recovery():
+    '''
+    Восстанавливает исходного строку по сжатой.
+    :return:
+    '''
+    with open('compression_text.txt', 'r') as f:
+        text = f.read()
+    position_sym = 0
+    recovery_text = ''
+
+    while position_sym < len(text)-1:
+        temp = ''
+        while text[position_sym].isdigit():
+            temp += str(text[position_sym])
+            position_sym+=1
+        count_letter = int(temp)
+
+        while count_letter>0:
+            recovery_text+=text[position_sym]
+            count_letter-=1
+        position_sym+=1
+    return recovery_text
+
+
+print(recovery())
+
