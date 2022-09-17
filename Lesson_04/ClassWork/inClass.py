2e6ef3bbf6fee82c15608e55a5c745425f613952
Yaroslav_Bezukladnikov <ybezukladnikov@gmail.com>
2022-09-17 18:38:33 +0300

    1 task

--- Lesson_04/ClassWork/inClass.py
+++ Lesson_04/ClassWork/inClass.py
@@ -2,11 +2,25 @@
 #
 # print(list_my)
 
-with open('test.txt', 'r') as f:
-    temp = f.read()
+# with open('test.txt', 'r') as f:
+#     temp = f.read()
+#
+# res = [(i, i**2) for i in map(lambda x:int(x),temp.split()) if i%2==0]
+#
+from math import factorial
+from random import random
 
-res = [(i, i**2) for i in map(lambda x:int(x),temp.split()) if i%2==0]
+numbers = list(map(lambda i: not i%2, (random.randint(1,100) for i in range(5))))
 
-# list_arr = list(map(int,temp.split()))
-# print(list_arr)
-print(res)
+print(numbers)
+
+numbers = [1,2,3]
+
+arr = list(map(factorial,numbers))
+
+
+# # list_arr = list(map(int,temp.split()))
+# # print(list_arr)
+# print(res)
+
+
