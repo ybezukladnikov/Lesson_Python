139b09c79e6a9b3b0e3fc1cfb74e04b914ecf91c
Yaroslav <yaros6666@yandex.ru>
2022-09-19 12:24:08 +0300

    done 1 task

--- Lesson_05/HomeWork/Task_01.py
+++ Lesson_05/HomeWork/Task_01.py
@@ -1,4 +1,9 @@
 # 1- Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 # 'абвгдейка - это передача' = >" - это передача"
 
+text = input('Input text: ')
+sub_string = input('Input sub_string: ')
 
+list_text = text.split()
+text_new =' '.join(filter(lambda x: sub_string not in x,list_text))
+print(f'Initial text => {text}\nSub string => {sub_string}\nNew text => {text_new}')
