8d73f6280c19f0aa9cd929d7bfa57767de5250c6
Yaroslav_Bezukladnikov <ybezukladnikov@gmail.com>
2022-10-11 19:10:31 +0300

    done 11 seminar

--- Lesson_09/lec_pip/test.py
+++ Lesson_09/lec_pip/test.py
@@ -1,13 +1,25 @@
-from progress.bar import Bar
-import time
-
-bar = Bar('Processing', max=20)
-for i in range(20):
-    time.sleep(1)
-    bar.next()
-bar.finish()
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+app = ApplicationBuilder().token("5647026158:AAHSap2UcNZoqwqUGG1uPMgS1hqRed0jrZ8").build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# list_m = [1,2,34,5,3,2]
+# plt.plot(list_m)
+#
+# plt.show()
 # import emoji
 # print(emoji.emojize('Python is :thumbs_up:'))
 
